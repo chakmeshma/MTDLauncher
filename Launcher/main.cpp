d5429ba187345d0798e375721939999f80d13dff
Majid Motallebikashani <chakmeshma@gmail.com>
2025-03-12 21:28:07 +0330

    Hardcoded Keys (Before Test)

--- Launcher/main.cpp
+++ Launcher/main.cpp
@@ -12,10 +12,48 @@ std::map<std::string, float> mapStrKey_Value{};
 
 void InitLoad() {
 	mapEditID_StrKey[IDC_EDIT1] = "JetAltitude";
+	mapEditID_StrKey[IDC_EDIT2] = "JetSpeed";
+	mapEditID_StrKey[IDC_EDIT3] = "MissileSpeedFactor";
+	mapEditID_StrKey[IDC_EDIT4] = "PropelForce";
+	mapEditID_StrKey[IDC_EDIT5] = "StickDistance";
+	mapEditID_StrKey[IDC_EDIT6] = "ShakeValue";
+	mapEditID_StrKey[IDC_EDIT7] = "RotationAngleLimit";
+	mapEditID_StrKey[IDC_EDIT8] = "DetectionRectMinArea";
+	mapEditID_StrKey[IDC_EDIT9] = "ColorSaturation;R";
+	mapEditID_StrKey[IDC_EDIT13] = "ColorSaturation;G";
+	mapEditID_StrKey[IDC_EDIT14] = "ColorSaturation;B";
+	mapEditID_StrKey[IDC_EDIT17] = "ColorSaturation;A";
+	mapEditID_StrKey[IDC_EDIT10] = "ColorContrast;R";
+	mapEditID_StrKey[IDC_EDIT15] = "ColorContrast;G";
+	mapEditID_StrKey[IDC_EDIT16] = "ColorContrast;B";
+	mapEditID_StrKey[IDC_EDIT18] = "ColorContrast;A";
+	mapEditID_StrKey[IDC_EDIT11] = "FilmGrainIntensity";
+	mapEditID_StrKey[IDC_EDIT12] = "CameraFOV";
 
 	void* iniReader = iniReaderInstantiate("C:\\settings.ini");
 
 	mapStrKey_Value["JetAltitude"] = iniReaderGetFloat(iniReader, "Geometry", "JetAltitude");
+	mapStrKey_Value["JetSpeed"] = iniReaderGetFloat(iniReader, "Geometry", "JetSpeed");
+	mapStrKey_Value["MissileSpeedFactor"] = iniReaderGetFloat(iniReader, "Geometry", "MissileSpeedFactor");
+	mapStrKey_Value["PropelForce"] = iniReaderGetFloat(iniReader, "Geometry", "PropelForce");
+	mapStrKey_Value["StickDistance"] = iniReaderGetFloat(iniReader, "Geometry", "StickDistance");
+	mapStrKey_Value["ShakeValue"] = iniReaderGetFloat(iniReader, "Geometry", "ShakeValue");
+	mapStrKey_Value["RotationAngleLimit"] = iniReaderGetFloat(iniReader, "Geometry", "RotationAngleLimit");
+	mapStrKey_Value["DetectionRectMinArea"] = iniReaderGetFloat(iniReader, "Geometry", "DetectionRectMinArea");
+
+	mapStrKey_Value["ColorSaturation;R"] = iniReaderGetFloatVector(iniReader, "Rendering", "ColorSaturation")[0];
+	mapStrKey_Value["ColorSaturation;G"] = iniReaderGetFloatVector(iniReader, "Rendering", "ColorSaturation")[1];
+	mapStrKey_Value["ColorSaturation;B"] = iniReaderGetFloatVector(iniReader, "Rendering", "ColorSaturation")[2];
+	mapStrKey_Value["ColorSaturation;A"] = iniReaderGetFloatVector(iniReader, "Rendering", "ColorSaturation")[3];
+
+	mapStrKey_Value["ColorContrast;R"] = iniReaderGetFloatVector(iniReader, "Rendering", "ColorContrast")[0];
+	mapStrKey_Value["ColorContrast;G"] = iniReaderGetFloatVector(iniReader, "Rendering", "ColorContrast")[1];
+	mapStrKey_Value["ColorContrast;B"] = iniReaderGetFloatVector(iniReader, "Rendering", "ColorContrast")[2];
+	mapStrKey_Value["ColorContrast;A"] = iniReaderGetFloatVector(iniReader, "Rendering", "ColorContrast")[3];
+
+	mapStrKey_Value["FilmGrainIntensity"] = iniReaderGetFloat(iniReader, "Rendering", "FilmGrainIntensity");
+	mapStrKey_Value["CameraFOV"] = iniReaderGetFloat(iniReader, "Rendering", "CameraFOV");
+
 
 	iniReaderDestroy(iniReader);
 }
