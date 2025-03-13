cffe7c0dcca7e06066fe33752c84029b9de57bc9
Majid Motallebikashani <chakmeshma@gmail.com>
2025-03-13 23:05:17 +0330

    Added Icon

--- Launcher/resource.h
+++ Launcher/resource.h
@@ -5,6 +5,7 @@
 #define IDOK2                           2
 #define IDOK3                           3
 #define IDD_DIALOG1                     101
+#define IDI_ICON1                       103
 #define IDC_EDIT1                       1001
 #define IDC_EDIT2                       1002
 #define IDC_EDIT3                       1003
@@ -28,7 +29,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
